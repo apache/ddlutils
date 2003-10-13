c9d6c465c408422a515b94bc3fb9c61b0c882ed7
Robert Burrell Donkin <rdonkin@apache.org>
2003-10-13 08:31:15 +0000

    Fixed typos in apache license
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/sql/trunk@147493 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/sql/model/TestColumn.java
+++ src/test/org/apache/commons/sql/model/TestColumn.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//sql/src/test/org/apache/commons/sql/model/TestColumn.java,v 1.4 2003/04/30 11:27:30 jstrachan Exp $
- * $Revision: 1.4 $
- * $Date: 2003/04/30 11:27:30 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//sql/src/test/org/apache/commons/sql/model/TestColumn.java,v 1.5 2003/10/13 08:28:30 rdonkin Exp $
+ * $Revision: 1.5 $
+ * $Date: 2003/10/13 08:28:30 $
  *
  * ====================================================================
  *
@@ -23,11 +23,11 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Commons", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -36,7 +36,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -57,7 +57,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  * 
- * $Id: TestColumn.java,v 1.4 2003/04/30 11:27:30 jstrachan Exp $
+ * $Id: TestColumn.java,v 1.5 2003/10/13 08:28:30 rdonkin Exp $
  */
 
 package org.apache.commons.sql.model;
@@ -72,7 +72,7 @@ import junit.framework.TestSuite;
  * Test harness for Column Class
  *
  * @author <a href="mailto:john@zenplex.com">John Thorhauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class TestColumn
      extends TestCase
