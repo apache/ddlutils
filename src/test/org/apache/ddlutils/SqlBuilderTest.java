4c360c9fbcf859b3a9ad85ff507ea331bcfa45a4
Thomas Dudziak <tomdz@apache.org>
2006-05-12 21:40:37 +0000

    Simplified the test case
    git-svn-id: https://svn.apache.org/repos/asf/db/ddlutils/trunk@405889 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/ddlutils/SqlBuilderTest.java
+++ src/test/org/apache/ddlutils/SqlBuilderTest.java
@@ -1,7 +1,7 @@
 package org.apache.ddlutils;
 
 /*
- * Copyright 1999-2006 The Apache Software Foundation.
+ * Copyright 2005-2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ package org.apache.ddlutils;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.ddlutils.Platform;
 import org.apache.ddlutils.model.Database;
+import org.apache.ddlutils.model.Table;
 import org.apache.ddlutils.platform.SqlBuilder;
 import org.apache.ddlutils.platform.TestPlatform;
 
@@ -32,63 +32,33 @@ import org.apache.ddlutils.platform.TestPlatform;
  */
 public class SqlBuilderTest extends TestBase
 {
-    /** The tested model. */
-    private static final String TEST_MODEL =
-        "<?xml version='1.0' encoding='ISO-8859-1'?>\n"+
-        "<database name='ddlutils'>\n"+
-        "  <table name='TestTable'>\n"+
-        "    <column name='id' autoIncrement='true' type='INTEGER' primaryKey='true'/>\n"+
-        "    <column name='name' type='VARCHAR' size='15'/>\n"+
-        "  </table>\n"+
-        "</database>";
-
     /**
-     * {@inheritDoc}
-     */
-    public void setUp()
-    {
-    }
-
-    /**
-     * Test the updateSql method.
+     * Tests the {@link SqlBuilder#getUpdateSql(Table, Map, boolean)} method.
      */
     public void testUpdateSql()
     {
+        final String modelXml =
+            "<?xml version='1.0' encoding='ISO-8859-1'?>\n"+
+            "<database name='ddlutils'>\n"+
+            "  <table name='TestTable'>\n"+
+            "    <column name='id' autoIncrement='true' type='INTEGER' primaryKey='true'/>\n"+
+            "    <column name='name' type='VARCHAR' size='15'/>\n"+
+            "  </table>\n"+
+            "</database>";
+
         TestPlatform platform   = new TestPlatform();
         SqlBuilder   sqlBuilder = platform.getSqlBuilder();
-        Database     database   = parseDatabaseFromString(TEST_MODEL);
+        Database     database   = parseDatabaseFromString(modelXml);
         Map          map        = new HashMap();
 
         map.put("name", "ddlutils");
         map.put("id", new Integer(0));
 
+        platform.setDelimitedIdentifierModeOn(true);
+        
         String sql = sqlBuilder.getUpdateSql(database.getTable(0), map, false);
 
         assertEquals("UPDATE \"TestTable\" SET \"name\" = 'ddlutils' WHERE \"id\" = '0'",
                      sql);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected String getDatabaseName()
-    {
-        return null;
-    }
-
-    /**
-     * The tested builder.
-     */
-    public class SqlBuilderImpl extends SqlBuilder
-    {
-        /**
-         * Creates a new instance.
-         * 
-         * @param platform The plaftform this builder belongs to
-         */
-        public SqlBuilderImpl(Platform platform)
-        {
-            super(platform);
-        }
     }
 }
