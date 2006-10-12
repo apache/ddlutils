7e94292f13c6982975e0f88d0fa7bb4b2937593c
Thomas Dudziak <tomdz@apache.org>
2006-10-12 16:28:02 +0000

    Updated the license header Removed the author tags
    git-svn-id: https://svn.apache.org/repos/asf/db/ddlutils/trunk@463305 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ddlutils/platform/SqlBuilder.java
+++ src/java/org/apache/ddlutils/platform/SqlBuilder.java
@@ -1,19 +1,22 @@
 package org.apache.ddlutils.platform;
 
 /*
- * Copyright 1999-2006 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import java.io.IOException;
@@ -83,11 +86,6 @@ import org.apache.ddlutils.util.MultiInstanceofPredicate;
  * it requires. Though often that can be quite complex when attempting to reuse code across many databases.
  * Hopefully only a small amount code needs to be changed on a per database basis.
  * 
- * TODO: It might be useful to add foreignkey analysis for creation/dropping of tables
- * 
- * @author James Strachan
- * @author John Marshall/Connectria
- * @author Thomas Dudziak
  * @version $Revision$
  */
 public abstract class SqlBuilder
