a3ebb9409ee1291283bb6dd2d6c9d2293dc7555c
Thomas Dudziak <tomdz@apache.org>
2008-11-04 17:42:23 +0000

    Minor tweak to the DDLUTILS-199 fix as suggested by Rijk van Haaften
    git-svn-id: https://svn.apache.org/repos/asf/db/ddlutils/trunk@711326 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/ddlutils/platform/postgresql/PostgreSqlBuilder.java
+++ src/main/java/org/apache/ddlutils/platform/postgresql/PostgreSqlBuilder.java
@@ -133,13 +133,7 @@ public class PostgreSqlBuilder extends SqlBuilder
     protected void writeColumnAutoIncrementStmt(Table table, Column column) throws IOException
     {
         print("UNIQUE DEFAULT nextval('");
-        if (getPlatform().isDelimitedIdentifierModeOn()) {
-            print("\"");
-        }
-        print(getConstraintName(null, table, column.getName(), "seq"));
-        if (getPlatform().isDelimitedIdentifierModeOn()) {
-            print("\"");
-        }
+        printIdentifier(getConstraintName(null, table, column.getName(), "seq"));
         print("')");
     }
 
@@ -166,13 +160,7 @@ public class PostgreSqlBuilder extends SqlBuilder
                     result.append(", ");
                 }
                 result.append("currval('");
-                if (getPlatform().isDelimitedIdentifierModeOn()) {
-                    result.append("\"");
-                }
-                result.append(getConstraintName(null, table, columns[idx].getName(), "seq"));
-                if (getPlatform().isDelimitedIdentifierModeOn()) {
-                    result.append("\"");
-                }
+                result.append(getDelimitedIdentifier(getConstraintName(null, table, columns[idx].getName(), "seq")));
                 result.append("') AS ");
                 result.append(getDelimitedIdentifier(columns[idx].getName()));
             }
